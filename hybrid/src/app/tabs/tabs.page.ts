490f1318775da4336007d6faf49a51b7d9fc4934
johrespi <139030531+Johrespi@users.noreply.github.com>
2024-08-07 10:39:18 -0500

    guia 25 by infrative

--- hybrid/src/app/tabs/tabs.page.ts
+++ hybrid/src/app/tabs/tabs.page.ts
@@ -1,7 +1,11 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square , peopleCircle} from 'ionicons/icons';
+import { triangle, ellipse, square, peopleCircle } from 'ionicons/icons';
+
+
+/* 1. Agregue la referencia al ícono camera  */
+import { camera } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -14,6 +18,9 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square,peopleCircle });
+    addIcons({ triangle, ellipse, square, peopleCircle });
+
+    /* 2. Agregue el ícono camera  */
+    addIcons({  camera });
   }
 }
