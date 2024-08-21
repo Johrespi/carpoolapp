f25754fa9ff94810895db177fcf663ed98067012
klashz <117329019+CristhianSantacruz@users.noreply.github.com>
2024-08-21 17:41:32 -0500

    mostrar alertas de erro al momentos de registrarse

--- hybrid/src/app/register/register.page.ts
+++ hybrid/src/app/register/register.page.ts
@@ -8,7 +8,7 @@ import { addIcons } from 'ionicons';
 import * as allIcons from 'ionicons/icons';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from '../services/authentication.service';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -37,14 +37,74 @@ export class RegisterPage implements OnInit {
     this.router.navigate(['/login']);
   }
 
-  onRegister() {
+  async onRegister() {
     if (this.registerForm.valid) {
       const email = this.registerForm.value.email;
       const password = this.registerForm.value.password;
-      this.authenticationService.register(email, password)
+      const confirmPassword = this.registerForm.value.confirmPassword;
+      if(password !== confirmPassword) {
+       
+        Swal.fire({
+          position : "bottom", 
+          icon: 'error',
+          text: 'Las contraseñas no coinciden',
+          heightAuto: false,
+          showConfirmButton: true,    
+        });
+        this.registerForm.patchValue({
+          password: '',
+          confirmPassword: ''
+        });
+        return;
+      }
+      await this.authenticationService.register(email, password)
       .then(() => {
+         Swal.fire({
+          position : "bottom", 
+          icon: 'success',
+          text: 'Registro exitoso',
+          heightAuto: false,
+          showConfirmButton: false,
+          timer:1000    
+        });
+        this.registerForm.reset();
         this.router.navigate(['/login']);
       }).catch((error) => {
+        
+        // error Passwood
+        // error email en uso
+        const message = error.message;
+        const condPassword = message.includes('auth/weak-password')
+        const condEmail = message.includes('auth/email-already-in-use');
+
+        if(condPassword){
+          Swal.fire({
+            position : "bottom", 
+            icon: 'error',
+            text: 'Contraseña muy débil, debe tener al menos 6 caracteres ',
+            heightAuto: false,
+            showConfirmButton: true,    
+          });
+        }
+        if(condEmail){
+          Swal.fire({
+            position : "bottom", 
+            icon: 'error',
+            text: 'El correo electrónico ya está en uso.',
+            heightAuto: false,
+            showConfirmButton: true,    
+          });
+        }
+        if(condPassword==false && condEmail==false){
+          Swal.fire({
+            position : "bottom", 
+            icon: 'error',
+            text: 'Ocurrio un error en el registro',
+            heightAuto: false,
+            showConfirmButton: true,    
+          });
+        }
+
         console.error(error);
       });
     }
